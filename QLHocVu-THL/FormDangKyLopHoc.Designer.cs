16ac9337780d7ea30a1ffea13193dae8faeb4b14
VUTRUONGTHANH <vutruongthanh25k@gmail.com>
2025-10-12 20:59:21 +0700

    Cập nhật Form đăng ký lớp học, thêm thủ tục sp_CapNhatTrangThaiLop và file README

---
+++ QLHocVu-THL/FormDangKyLopHoc.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace QLHocVu_THL
+{
+    partial class FormDangKyLopHoc
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbLop = new System.Windows.Forms.ComboBox();
+            this.txtMaSV = new System.Windows.Forms.TextBox();
+            this.txtMaHK = new System.Windows.Forms.TextBox();
+            this.btnDangKy = new System.Windows.Forms.Button();
+            this.dgvLop = new System.Windows.Forms.DataGridView();
+            this.cbHocKy = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLop)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cbLop
+            // 
+            this.cbLop.FormattingEnabled = true;
+            this.cbLop.Location = new System.Drawing.Point(78, 93);
+            this.cbLop.Name = "cbLop";
+            this.cbLop.Size = new System.Drawing.Size(121, 24);
+            this.cbLop.TabIndex = 0;
+            this.cbLop.Text = "chọn lớp";
+            // 
+            // txtMaSV
+            // 
+            this.txtMaSV.Location = new System.Drawing.Point(260, 62);
+            this.txtMaSV.Name = "txtMaSV";
+            this.txtMaSV.Size = new System.Drawing.Size(126, 22);
+            this.txtMaSV.TabIndex = 1;
+            this.txtMaSV.Text = "mã sinh viên";
+            // 
+            // txtMaHK
+            // 
+            this.txtMaHK.Location = new System.Drawing.Point(266, 136);
+            this.txtMaHK.Name = "txtMaHK";
+            this.txtMaHK.Size = new System.Drawing.Size(100, 22);
+            this.txtMaHK.TabIndex = 2;
+            // 
+            // btnDangKy
+            // 
+            this.btnDangKy.Location = new System.Drawing.Point(213, 216);
+            this.btnDangKy.Name = "btnDangKy";
+            this.btnDangKy.Size = new System.Drawing.Size(73, 41);
+            this.btnDangKy.TabIndex = 3;
+            this.btnDangKy.Text = "nút đăng ký";
+            this.btnDangKy.UseVisualStyleBackColor = true;
+            this.btnDangKy.Click += new System.EventHandler(this.btnDangKy_Click);
+            // 
+            // dgvLop
+            // 
+            this.dgvLop.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvLop.Location = new System.Drawing.Point(364, 213);
+            this.dgvLop.Name = "dgvLop";
+            this.dgvLop.RowHeadersWidth = 51;
+            this.dgvLop.RowTemplate.Height = 24;
+            this.dgvLop.Size = new System.Drawing.Size(410, 229);
+            this.dgvLop.TabIndex = 4;
+            // 
+            // cbHocKy
+            // 
+            this.cbHocKy.FormattingEnabled = true;
+            this.cbHocKy.Location = new System.Drawing.Point(67, 25);
+            this.cbHocKy.Name = "cbHocKy";
+            this.cbHocKy.Size = new System.Drawing.Size(121, 24);
+            this.cbHocKy.TabIndex = 5;
+            this.cbHocKy.Text = "chọn học kỳ";
+            this.cbHocKy.SelectedIndexChanged += new System.EventHandler(this.cbHocKy_SelectedIndexChanged);
+            // 
+            // FormDangKyLopHoc
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cbHocKy);
+            this.Controls.Add(this.dgvLop);
+            this.Controls.Add(this.btnDangKy);
+            this.Controls.Add(this.txtMaHK);
+            this.Controls.Add(this.txtMaSV);
+            this.Controls.Add(this.cbLop);
+            this.Name = "FormDangKyLopHoc";
+            this.Text = "DangKyLopHocDTO";
+            this.Load += new System.EventHandler(this.FormDangKyLopHoc_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLop)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbLop;
+        private System.Windows.Forms.TextBox txtMaSV;
+        private System.Windows.Forms.TextBox txtMaHK;
+        private System.Windows.Forms.Button btnDangKy;
+        private System.Windows.Forms.DataGridView dgvLop;
+        private System.Windows.Forms.ComboBox cbHocKy;
+    }
+}
---
+++ QLHocVu-THL/FormDangKyLopHoc.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QLHocVu_THL
+{
+    public partial class FormDangKyLopHoc : Form
+    {
+        DangKyLopHocBUS bus = new DangKyLopHocBUS();
+        public FormDangKyLopHoc()
+        {
+            InitializeComponent();
+        }
+
+        private void FormDangKyLopHoc_Load(object sender, EventArgs e)
+        {
+            // Giả sử load danh sách học kỳ vào ComboBox
+            DataTable dtHK = new DataTable();
+            dtHK.Columns.Add("MaHK");
+            dtHK.Columns.Add("TenHK");
+
+            dtHK.Rows.Add("HK1", "Học kỳ 1");
+            dtHK.Rows.Add("HK2", "Học kỳ 2");
+
+            cbHocKy.DataSource = dtHK;
+            cbHocKy.DisplayMember = "TenHK";
+            cbHocKy.ValueMember = "MaHK";
+        }
+
+        private void btnDangKy_Click(object sender, EventArgs e)
+        {
+            string maDangKy = Guid.NewGuid().ToString().Substring(0, 8);
+            string maSV = txtMaSV.Text;
+            string maLop = cbLop.SelectedValue.ToString();
+            string maHK = cbHocKy.SelectedValue.ToString();
+
+            DangKyLopHocDTO dk = new DangKyLopHocDTO(maDangKy, maSV, maLop, maHK);
+
+            if (bus.DangKy(dk))
+                MessageBox.Show("✅ Đăng ký lớp học thành công!");
+            else
+                MessageBox.Show("❌ Đăng ký thất bại hoặc trùng lớp!");
+        }
+
+        private void cbHocKy_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string maHK = cbHocKy.SelectedValue.ToString();
+            DataTable dtLop = bus.LayLopTheoHocKy(maHK);
+
+            dgvLop.DataSource = dtLop;
+            cbLop.DataSource = dtLop;
+            cbLop.DisplayMember = "TenLop";
+            cbLop.ValueMember = "MaLop";
+        }
+    }
+}
