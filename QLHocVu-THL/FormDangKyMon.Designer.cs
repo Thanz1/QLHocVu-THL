9437f6d2bdd71dad84670d14c2b717688aecf84f
ADMIN <truongthanhlam27072005@gmail.com>
2025-10-15 23:54:32 +0700

    thêm đăng ký môn học

--- QLHocVu-THL/FormDangKyMon.Designer.cs
+++ QLHocVu-THL/FormDangKyMon.Designer.cs
@@ -28,56 +28,121 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnHuydangky = new System.Windows.Forms.Button();
+            this.btnDangky = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.S = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             this.SuspendLayout();
+            // 
+            // btnHuydangky
+            // 
+            this.btnHuydangky.Location = new System.Drawing.Point(581, 378);
+            this.btnHuydangky.Name = "btnHuydangky";
+            this.btnHuydangky.Size = new System.Drawing.Size(102, 38);
+            this.btnHuydangky.TabIndex = 1;
+            this.btnHuydangky.Text = "Hủy đăng ký";
+            this.btnHuydangky.UseVisualStyleBackColor = true;
+            // 
+            // btnDangky
+            // 
+            this.btnDangky.Location = new System.Drawing.Point(376, 378);
+            this.btnDangky.Name = "btnDangky";
+            this.btnDangky.Size = new System.Drawing.Size(102, 37);
+            this.btnDangky.TabIndex = 2;
+            this.btnDangky.Text = "Đăng ký";
+            this.btnDangky.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(17, 86);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(1050, 262);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // S
+            // 
+            this.S.AutoSize = true;
+            this.S.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.S.Location = new System.Drawing.Point(12, 9);
+            this.S.Name = "S";
+            this.S.Size = new System.Drawing.Size(197, 29);
+            this.S.TabIndex = 4;
+            this.S.Text = "Đăng ký môn học";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(14, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(234, 16);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Doanh sách  môn học mở cho đăng ký";
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(17, 440);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(1041, 213);
+            this.dataGridView2.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(14, 411);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(203, 16);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Doanh sách môn học đã đăng ký";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(506, 303);
+            this.button1.Location = new System.Drawing.Point(749, 381);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Size = new System.Drawing.Size(89, 34);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Xuất phiếu đăng ký";
             this.button1.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(347, 303);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(220, 303);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
             // 
             // FormDangKyMon
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
+            this.ClientSize = new System.Drawing.Size(1108, 676);
             this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.S);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnDangky);
+            this.Controls.Add(this.btnHuydangky);
             this.Name = "FormDangKyMon";
             this.Text = "FormDangKyMon";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-
+        private System.Windows.Forms.Button btnHuydangky;
+        private System.Windows.Forms.Button btnDangky;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label S;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
     }
 }
